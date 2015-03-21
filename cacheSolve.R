2cd255470300a3e47155f2af7d58fe18452d02c5
Foxtrot22 <samhern89@gmail.com>
2015-03-21 17:52:59 -0500

    Programming Assignment 2
    Contains code for Programming Assignment 2.

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,1 +1,17 @@
-Test2
+## "cacheSolve" calculates the inverse of the matrix from the
+## "makeCacheMatrix" function. It first checks to see if the
+##inverse has already been calculated. If so, it gets the matrix
+##inverse from the cache and skips the computation. Otherwise, it
+##calculates the inverse and sets the inverse in the cache.
+cacheSolve <- function(x, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(max(is.na(m))==0) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+}
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,1 +1,21 @@
-Test
+## "makeCacheMatrix" creates a list of functions
+## that do the following:
+## 1. Set the matrix
+## 2. Get the matrix
+## 3. Set the inverse of the matrix
+## 4. Get the inverse of the matrix
+makeCacheMatrix <- function(x = matrix()) {
+    
+    m <- matrix(NA, nrow=dim(x)[1], ncol=dim(x)[2])
+    set <- function(y) {
+        x <<- y
+        m <<- matrix(NA, nrow=dim(x)[1], ncol=dim(x)[2])
+    }
+    get <- function() x
+    setinverse <- function(inv) m <<- inv
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
+}
